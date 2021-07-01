046360951595a225400a89af69406d8800517d98
Fhjull <dillab@list.ru>
2021-07-01 20:28:26 +0300

    added drawables

--- app/src/main/java/ru/dillab/recyclerview/MainActivity.kt
+++ app/src/main/java/ru/dillab/recyclerview/MainActivity.kt
@@ -6,7 +6,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -26,22 +29,25 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-data class Item(val stringResourceId: Int)
+data class Item(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+    )
 
 class DataSource {
 
     fun loadStrings(): List<Item> {
         return listOf(
-            Item(R.string.string_1),
-            Item(R.string.string_2),
-            Item(R.string.string_3),
-            Item(R.string.string_4),
-            Item(R.string.string_5),
-            Item(R.string.string_6),
-            Item(R.string.string_7),
-            Item(R.string.string_8),
-            Item(R.string.string_9),
-            Item(R.string.string_10)
+            Item(R.string.string_1, R.drawable.image1),
+            Item(R.string.string_2, R.drawable.image2),
+            Item(R.string.string_3, R.drawable.image3),
+            Item(R.string.string_4, R.drawable.image4),
+            Item(R.string.string_5, R.drawable.image5),
+            Item(R.string.string_6, R.drawable.image6),
+            Item(R.string.string_7, R.drawable.image7),
+            Item(R.string.string_8, R.drawable.image8),
+            Item(R.string.string_9, R.drawable.image9),
+            Item(R.string.string_10, R.drawable.image10)
         )
     }
 }
@@ -61,7 +67,8 @@ class ItemAdapter(
      *  Each data item is just an [Item] object.
      */
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById(R.id.item_layout_id)
+        val textView: TextView = view.findViewById(R.id.text_layout_id)
+        val imageView: ImageView = view.findViewById(R.id.image_layout_id)
     }
 
     /**
@@ -81,6 +88,7 @@ class ItemAdapter(
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = data[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     /**
